8d9d804375902bedd301dd6b8431b1f1c581fd6e
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 15:49:51 +0300

    Update Worker.cpp
    Initial project structure

--- Worker.cpp
+++ Worker.cpp
@@ -1,1 +1,11 @@
+#include "Worker.h"
+
+Worker::Worker(int id, int arrival, int service) {
+    this->id = id;
+    this->arrivalTime = arrival;
+    this->serviceTime = service;
+    this->startTime = -1;
+}
+
+
 
