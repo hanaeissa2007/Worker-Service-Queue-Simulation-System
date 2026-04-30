ba523df0d8fff81a032942a216a5e1084366bbc1
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 15:52:37 +0300

    Update Server.h
    Initial project structure

--- Server.h
+++ Server.h
@@ -1,1 +1,18 @@
+#ifndef SERVER_H
+#define SERVER_H
 
+#include "Worker.h"
+
+class Server {
+public:
+    bool busy;
+    int remainingTime;
+    Worker currentWorker;
+
+    Server();
+
+    void assignWorker(Worker w, int currentTime);
+    void process();
+};
+
+#endif
