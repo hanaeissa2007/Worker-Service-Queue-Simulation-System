239a51c01dd83278a8d2509148cfc380dd84f74d
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 15:53:08 +0300

    Update Server.cpp
    Initial project structure

--- Server.cpp
+++ Server.cpp
@@ -1,1 +1,22 @@
+#include "Server.h"
 
+Server::Server() {
+    busy = false;
+    remainingTime = 0;
+}
+
+void Server::assignWorker(Worker w, int currentTime) {
+    busy = true;
+    currentWorker = w;
+    currentWorker.startTime = currentTime;
+    remainingTime = w.serviceTime;
+}
+
+void Server::process() {
+    if (busy) {
+        remainingTime--;
+        if (remainingTime <= 0) {
+            busy = false;
+        }
+    }
+}
