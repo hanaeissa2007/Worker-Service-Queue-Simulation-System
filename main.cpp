4805bdabd6f278d4eab9b9fd3b19abe6c6d20925
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 16:10:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 int main() {
     cout << "Queue Simulation System Started..." << endl;
-    Simulation sim(20, 10, 2); // 2 servers 🔥
+   Simulation sim1(20, 10, 1);
+   Simulation sim2(20, 10, 2);
+//Then say in report:
+//“We compared system performance with different numbers of servers.”
     sim.run();
     return 0;
 }
