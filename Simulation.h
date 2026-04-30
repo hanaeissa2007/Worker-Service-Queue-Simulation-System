40db9e1e1477a2b1ed39efe94cb3590d70d464e7
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 15:53:45 +0300

    Update Simulation.h
    Initial project structure

--- Simulation.h
+++ Simulation.h
@@ -1,1 +1,19 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
 
+#include "Queue.h"
+#include "Server.h"
+
+class Simulation {
+private:
+    int time;
+    int maxTime;
+    Queue queue;
+    Server server;
+
+public:
+    Simulation(int simTime, int queueSize);
+    void run();
+};
+
+#endif
