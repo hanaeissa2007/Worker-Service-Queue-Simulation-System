7d19b1479a5cd289b24eebdf16439df48ccd0afa
Hana Eissa <hanaeissa725@gmail.com>
2026-05-01 18:06:13 +0300

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -16,61 +16,17 @@ public:
         count = 0;
         Worker = new int[maxSize];
     }
-~Queue(){
-    delete[]Worker;
-}
-void initializeQueue(){
-        front = 0;
-        rear = -1;
-        count = 0;
-        for (int i = 0; i < maxSize; i++){
-            Worker[i] = 0;
-        }
-    cout<< "Queue is initialized!" << endl;
-    }
-    bool isEmpty(){
-        return count == 0;
-    }
-    bool isFull(){
-         return count == maxSize;
-    }
-    void enqueue(int item){
-        if(isFull()){
-            cout<< "The queue is full" << endl;
-            return false;
-        }
-        rear = (rear + 1) % maxSize;
-        Worker[rear] = item;
-        count++;
-        cout<< item << " is added" << endl;
-        return true;
-    }
-    Worker dequeue(){
-         if (isEmpty()){
-            cout<< "The queue is empty" << endl;
-            return -1;
-        }
-        int deletedItem = Worker[front];
-        Worker[front] = 0;
-        front = (front + 1) % maxSize;
-        count--;
-        cout<< deletedItem << " is deleted" << endl;
-        return deletedItem;
-    }
-void display(){
-        if (isEmpty()){
-            cout<< "The queue is empty" << endl;
-            return;
-        }
-        cout<< "The queue contents are (from front to rear):" << endl;
-        int index = front;
-        for(int i = 0; i < count; i++){
-            cout<< Worker[index] << " ";
-            index = (index + 1) % maxSize;
-        }
-        cout<< endl;
-        cout<< "Front: " << front << ", Rear: " << rear << ", Count: " << count
-        << ", Maximum size: " << maxSize << endl;
+
+void initializeQueue();
+
+    bool isEmpty();
+       
+    bool isFull();
+    
+    void enqueue(int item);
+    Worker dequeue();
+    void display();
+       
     }
 
 };
